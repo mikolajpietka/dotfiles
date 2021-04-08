96085239ad950c440aa7c5f3960db09307b19a95
Mikołaj Piętka <mikolaj.pietka98@gmail.com>
2021-04-08 19:47:14 +0200

    Added Polkit and cleaned

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,16 +1,10 @@
-#!/bin/sh
+#!/usr/bin/bash
 
 # Autostart for Qtile
-function run {
-  if ! pgrep $1 ;
-  then
-    $@&
-  fi
-}
 
 picom &
 feh --bg-scale $HOME/wallpapers/$(ls -I "README.md" $HOME/wallpapers/ | shuf -n1) &
-
-run nm-applet &
+nm-applet &
 numlockx on &
-run volumeicon &
+volumeicon &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
