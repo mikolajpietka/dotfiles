a35a0222ba9c9c0d9a641a31974b1c940321c9c7
Mikołaj Piętka <mikolaj.pietka98@gmail.com>
2021-04-18 16:30:27 +0200

    Changes

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -18,7 +18,7 @@
 wallpapers_folder = "~/wallpapers/minimal"
 
 picom &
-feh --bg-scale $wallpapers_folder$(ls -I "README.md" $wallpapers_folder | shuf -n1) &
+feh --bg-scale ~/wallpapers/minimal/$(ls -I "README.md" ~/wallpapers/minimal | shuf -n1) &
 numlockx on &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 timedatectl set-timezone Europe/Warsaw &
