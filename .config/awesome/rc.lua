44b0042d1c2178f07e35cde5c09eb34c3185136e
Mikołaj Piętka <mikolaj.pietka98@gmail.com>
2021-09-01 21:32:42 +0200

    Changes

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,117 +1,79 @@
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
-
--- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
 require("awful.autofocus")
--- Widget and layout library
 local wibox = require("wibox")
--- Theme handling library
 local beautiful = require("beautiful")
--- Notification library
 local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
+-- Error handling
 if awesome.startup_errors then
     naughty.notify({ preset = naughty.config.presets.critical,
                      title = "Oops, there were errors during startup!",
                      text = awesome.startup_errors })
 end
-
--- Handle runtime errors after startup
 do
     local in_error = false
     awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
         if in_error then return end
         in_error = true
-
         naughty.notify({ preset = naughty.config.presets.critical,
                          title = "Oops, an error happened!",
                          text = tostring(err) })
         in_error = false
     end)
 end
--- }}}
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
+-- THEME --
 beautiful.init("~/.config/awesome/themes/default/theme.lua")
 
--- This is used later as the default terminal and editor to run.
+-- VARIABLES --
 terminal = "alacritty"
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
-
--- Start script
-awful.spawn.with_shell("~/.config/awesome/autostart.sh")
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
--- Table of layouts to cover with awful.layout.inc, order matters.
+-- AUTOSTART --
+awful.spawn.with_shell("~/.config/scripts/autostart.sh")
+
+-- LAYOUTS --
 awful.layout.layouts = {
     awful.layout.suit.tile,
     awful.layout.suit.floating,
-    -- awful.layout.suit.tile.left,
-    -- awful.layout.suit.tile.bottom,
-    -- awful.layout.suit.tile.top,
-    -- awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
-    -- awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
-    -- awful.layout.suit.max.fullscreen,
-    -- awful.layout.suit.magnifier,
-    -- awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
+    awful.layout.suit.spiral.dwindle,
 }
--- }}}
 
--- {{{ Menu
--- Create a launcher widget and a main menu
+-- MENU --
 myawesomemenu = {
-   { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", function() awesome.quit() end },
+    {"Hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end},
+    {"Edit config", editor_cmd .. " " .. awesome.conffile},
+    {"Restart", awesome.restart},
 }
 
 exitmenu = {
-    { "logout", "killall awesome" },
-    { "shutdown", "shutdown now" },
+    {"Shutdown", "shutdown now"},
+    {"Logout", function() awesome.quit() end},
+    {"Reboot", "reboot"},
 }
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", terminal },
-                                    { "exit", exitmenu },
-                                  }
-                        })
+mymainmenu = awful.menu({items = {
+    {"Awesome", myawesomemenu, beautiful.awesome_icon},
+    {"Terminal", terminal},
+    {"Jgmenu", "jgmenu_run"},
+    {"Firefox", "firefox"},
+    {"Rofi", "rofi -show drun"},
+    {"Exit", exitmenu},
+}})
 
+-- BAR --
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
                                      menu = mymainmenu })
 
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
--- }}}
-
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
 
 -- {{{ Wibar
 -- Create a textclock widget
