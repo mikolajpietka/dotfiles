de92441166889e30a6969cbb4fa396112a5b1326
Mikołaj Piętka <mikolaj.pietka98@gmail.com>
2021-09-22 21:16:58 +0200

    Szkoda gadać...

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -41,10 +41,9 @@ awful.spawn.with_shell("~/.config/scripts/autostart.sh")
 
 -- LAYOUTS --
 awful.layout.layouts = {
-    awful.layout.suit.tile,
+    awful.layout.suit.spiral.dwindle,
     awful.layout.suit.floating,
     awful.layout.suit.max,
-    awful.layout.suit.spiral.dwindle,
 }
 
 -- MENU --
@@ -140,7 +139,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag({ "1", "2", "3", "4", "5", "6", "7" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
